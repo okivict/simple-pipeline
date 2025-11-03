37a053b5ee0bb7284d6a6a4947829f8378e0911c
okivict <oki.victoria@jubelio.com>
2025-11-04 01:12:31 +0700

    ubah isi app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "sip aman ampe sini"
+    return "test kedua sebelum pake webhook ges"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
