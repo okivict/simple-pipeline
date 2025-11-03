287ad2a10311548ab92b37061156e0fb666fd129
okivict <oki.victoria@jubelio.com>
2025-11-04 01:28:10 +0700

    ubah nama file .jpeg  lagi

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def show_image():
-    return '<img src="/static/images.jpeg">'
+    return '<img src="/static/1.jpg">'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
