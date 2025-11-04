6a39a5e526ee64ba4eb923a33418fad4110f961f
okivict <oki.victoria@jubelio.com>
2025-11-04 15:06:51 +0700

    add gambar

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-#def show_image():
-#   return '<img src="/static/1.jpg">'
+def show_image():
+   return '<img src="/static/1.jpg">'
 def hello():
     return '<h1>BERHASIL</h1>'
 
