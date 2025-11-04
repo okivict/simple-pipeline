1b6e15e31b0a035e80ec9a2fde8ff009a4603fe9
okivict <oki.victoria@jubelio.com>
2025-11-04 15:08:27 +0700

    add gambar 2

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 @app.route('/')
 def show_image():
    return '<img src="/static/1.jpg">'
-def hello():
-    return '<h1>BERHASIL</h1>'
+#def hello():
+#   return '<h1>BERHASIL</h1>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
