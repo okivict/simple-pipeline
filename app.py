a9bdd29d9a923a2abc1233aab4f4005307012dc5
okivict <oki.victoria@jubelio.com>
2025-11-04 15:11:26 +0700

    balik ke 1

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def show_image():
-   return '<img src="/static/1.jpg">'
-#def hello():
-#   return '<h1>BERHASIL</h1>'
+#def show_image():
+#  return '<img src="/static/1.jpg">'
+def hello():
+   return '<h1>BERHASIL</h1>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
